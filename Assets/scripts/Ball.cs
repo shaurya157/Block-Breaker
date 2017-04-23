c192dad4d028edc363f7840d6747b7e91feb208a
Shaurya Sinha <shaurya157@gmail.com>
2017-04-24 01:08:51 +0300

    physics engine done

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted){
+			this.transform.position = paddle.transform.position +  paddleToBallVector;
+			if(Input.GetMouseButtonDown(0)){
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+				hasStarted = true;
+			}
+		}
+	
+	}
+}
---
+++ Assets/scripts/LoseCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+	
+	public LevelManager levelmanager;
+	
+	void OnTriggerEnter2D(Collider2D trigger){
+		levelmanager.LoadLevel("lose");
+	}
+	
+	void OnCollisionEnter2D(Collision2D collider){
+		print (collider);
+		print ("collided!");
+	}
+}
