282a3780fccb44e6e9429dc04deb7c9ca58ab670
Shaurya Sinha <shaurya157@gmail.com>
2017-04-23 16:26:45 +0300

    first commit

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Level lod requested" + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log ("Level quit requested");
+//		Application.Quit();
+	}
+}
---
+++ Assets/scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	void Awake (){
+		if (instance){
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(this);
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
